02f785ad293e1056a3f89cb09bc8e7b5c8b66cea
nifantov <m.v.nifantov@gmail.com>
2022-04-19 23:33:02 +0300

    final

--- script.js
+++ script.js
@@ -1,31 +1,51 @@
 let myLibrary = [];
 
+
 class Book {
-    constructor(title, author, pages, read) {
+    constructor(
+      title = "Unknown", 
+      author = "Unknown", 
+      pages = 0, 
+      read = false
+      ) {
         this.title = title,
-            this.author = author,
-            this.pages = pages,
-            this.read = read;
+        this.author = author,
+        this.pages = pages,
+        this.read = read;
     }
 };
-
 
 //current DOM
 
 const grid = document.getElementById("grid");
 const addBookBtn = document.getElementById("addBook");
 const openFormBtn = document.getElementById("openForm");
+const form = document.querySelector("form");
+
+
 
 //buttons listeners
-addBookBtn.addEventListener("click", () => addBookToLibrary(getBook()));
+addBookBtn.addEventListener("click", () => addBook(getBook()));
 openFormBtn.addEventListener("click", () => openForm());
 
 
-
 //add new book to the library
-function addBookToLibrary(book) {
-    myLibrary.push(book);
-    console.log(myLibrary);
+function addBook(book) {
+  if (checkBook(book.title) !== false) {
+    return alert("You already have it!");
+  }
+  myLibrary.push(book);
+  addBookCard(book);
+  closeForm();
+}
+
+//check book in library 
+function checkBook(title) {
+  return myLibrary.some((book) => book.title === title);
+};
+
+//add new book card
+function addBookCard(book) {
 
     //new DOM
     const card = document.createElement("div");
@@ -37,15 +57,22 @@ function addBookToLibrary(book) {
 
     //add classes to new DOM
     card.classList.add("card");
-    read.classList.add("read");
     cross.classList.add("cross");
 
     //add content to new DOM
     title.textContent = `${book.title}`;
     author.textContent = `${book.author}`;
     pages.textContent = `${book.pages} pages`;
-    read.textContent = `${book.read}`;
     cross.textContent = "x";
+
+    if (book.read) {
+      read.textContent = "Read";
+      read.classList.add("read");
+    }
+    else {
+      read.textContent = "Not read yet";
+      read.classList.add("noread");
+    }
 
     //add containers
     grid.appendChild(card);
@@ -56,12 +83,35 @@ function addBookToLibrary(book) {
     card.appendChild(cross);
 
     //buttons listeners
-    //cross.addEventListener("click", () => remove);
+    cross.addEventListener("click", () => remove(book.title));
+    read.addEventListener("click", () => toggleRead(book.title))
+};
+
+//toggle read
+function toggleRead(title) {
+  console.log (title)
+  myLibrary = myLibrary.map(book => {
+    if (book.title === title) {
+      if (book.read) {
+        book.read = false;
+        return book;
+      }
+      else {
+        book.read = true;
+        return book;
+      }
+    }
+    else {
+      return book;
+    }
+  })
+  updateGrid();
 };
 
 //form
 function openForm() {
     document.getElementById("popup").style.display = "block";
+    form.reset();
   }
   
   function closeForm() {
@@ -69,15 +119,32 @@ function openForm() {
   }
 //
 
+
 function getBook () {
-    const title = document.getElementById('title').value
-    const author = document.getElementById('author').value
-    const pages = document.getElementById('pages').value
-    //const isRead = document.getElementById('read').checked
-    return new Book(title, author, pages, /* isRead */)
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const read = document.getElementById('readAsk').checked;
+    return new Book(title, author, pages, read)
   }
 
 const firstBook = new Book ("good book", "author", "66", "have read");
 
+//remove book
+function remove(title) {
+  myLibrary = myLibrary.filter(book => book.title !== title);
+  updateGrid();
+};
+
+//update grid
+function updateGrid() {
+  resetGrid();
+  myLibrary.forEach(book => addBookCard(book)); 
+};
+
+//clear grid
+function resetGrid() {
+  grid.innerHTML = "";
+};
 
 
